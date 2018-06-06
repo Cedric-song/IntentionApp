39acb39f478fd61a97687227f8242731d4229091
Cedric Song <songbin@silots.com>
2018-06-07 01:06:41 +0800

    build

--- config/index.js
+++ config/index.js
@@ -58,7 +58,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dist/h5'),
     // assetsRoot: path.resolve(__dirname, '../../www'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
