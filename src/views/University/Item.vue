3fdbdc37a84ec0b5755a796cc54f4eac3b52c335
Cedric Song <songbin@silots.com>
2018-06-21 01:33:10 +0800

    build

--- src/views/University/Item.vue
+++ src/views/University/Item.vue
@@ -1,30 +1,32 @@
 <template>
-  <div v-if="Object.keys(this.form).length !== 0">
+  <div>
     <van-nav-bar :title="$route.query.name" left-text="返回" right-text="" left-arrow @click-left="$router.back()" />
+    <template v-if="Object.keys(this.form).length !== 0">
 
-    <wap-subtitle subtitle="院校信息" style="margin-top: 20px;"></wap-subtitle>
-    <van-row :gutter="20" class="info">
-      <van-col span="24" class="info-item" style="text-align:center;height:130px;"><img :src="form.logo" alt=""></van-col>
-      <van-col span="24" class="info-item">院校名称： {{$route.query.name}}</van-col>
-      <van-col span="24" class="info-item">院校代码： {{form.code}}</van-col>
-      <van-col span="24" class="info-item">院校级别： {{form.eduLevel}}</van-col>
-      <van-col span="24" class="info-item">所在省市： {{form.provinceName}}</van-col>
-      <van-col span="24" class="info-item">详细地址： {{form.address}}</van-col>
-      <!-- <van-col span="24" class="info-item">所在省市： {{form.provinceName}}</van-col> -->
-      <van-col span="24" class="info-item">分类： {{form.category}}</van-col>
-      <van-col span="24" class="info-item">类别： {{form.nature}}</van-col>
-      <van-col span="24" class="info-item">招办电话： {{form.callPhone}}</van-col>
-      <van-col span="24" class="info-item">邮箱： {{form.emailAddress}}</van-col>
-      <van-col span="24" class="info-item">官网：
-        <span class="item-blue">{{form.webSite}}</span>
-      </van-col>
-    </van-row>
-    <template v-if="intro !== ''">
-      <wap-subtitle subtitle="院校简介" style="margin-top: 20px;"></wap-subtitle>
+      <wap-subtitle subtitle="院校信息" style="margin-top: 20px;"></wap-subtitle>
       <van-row :gutter="20" class="info">
-        <van-col span="24" class="info-item info-intro" :class="{'showmore': showMore}" v-html="intro"></van-col>
-        <van-col span=" 24 " class="info-item info-more " @click.native="showMore=! showMore "> {{showMore ? '收起' : '查看更多'}}</van-col>
+        <van-col span="24" class="info-item" style="text-align:center;height:130px;"><img :src="form.logo" alt=""></van-col>
+        <van-col span="24" class="info-item">院校名称： {{$route.query.name}}</van-col>
+        <van-col span="24" class="info-item">院校代码： {{form.code}}</van-col>
+        <van-col span="24" class="info-item">院校级别： {{form.eduLevel}}</van-col>
+        <van-col span="24" class="info-item">所在省市： {{form.provinceName}}</van-col>
+        <van-col span="24" class="info-item">详细地址： {{form.address}}</van-col>
+        <!-- <van-col span="24" class="info-item">所在省市： {{form.provinceName}}</van-col> -->
+        <van-col span="24" class="info-item">分类： {{form.category}}</van-col>
+        <van-col span="24" class="info-item">类别： {{form.nature}}</van-col>
+        <van-col span="24" class="info-item">招办电话： {{form.callPhone}}</van-col>
+        <van-col span="24" class="info-item">邮箱： {{form.emailAddress}}</van-col>
+        <van-col span="24" class="info-item">官网：
+          <span class="item-blue">{{form.webSite}}</span>
+        </van-col>
       </van-row>
+      <template v-if="intro !== ''">
+        <wap-subtitle subtitle="院校简介" style="margin-top: 20px;"></wap-subtitle>
+        <van-row :gutter="20" class="info">
+          <van-col span="24" class="info-item info-intro" :class="{'showmore': showMore}" v-html="intro"></van-col>
+          <van-col span=" 24 " class="info-item info-more " @click.native="showMore=! showMore "> {{showMore ? '收起' : '查看更多'}}</van-col>
+        </van-row>
+      </template>
     </template>
 
   </div>
