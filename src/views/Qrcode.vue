8201a79a0622690ce733b5ed32b359775447b771
Cedric Song <songbin@silots.com>
2018-06-25 16:40:04 +0800

    build

--- src/views/Qrcode.vue
+++ src/views/Qrcode.vue
@@ -3,7 +3,7 @@
     <van-nav-bar title="咨询热线" left-text="返回" right-text="" left-arrow @click-left="$router.back()" />
     <van-row>
       <van-col span="24" class="tips">
-        如果您在志愿宝平台使用过程中有任何问题，欢迎您扫描下面二维码，添加在线客服为好友进行咨询。
+        如果您在志愿宝平台使用过程中有任何问题，欢迎您扫描下面二维码，添加在线客服为好友进行咨询，也可拨打10000号。
       </van-col>
     </van-row>
     <img src="/static/imgs/qr.jpg" alt="" style="width: 50%;margin-top: 50px;">
